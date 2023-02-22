77838054426aff23fcbe2d01cab76cac712b9503
Yash <2105595@kiit.ac.in>
2023-02-22 22:39:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs=require("ejs");
+const mongoose=require('mongoose');
+const encrypt=require('mongoose-encryption');
+const mongooseFieldEncryption = require("mongoose-field-encryption").fieldEncryption;
+
+
+const app=express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+
+const userSchema=new mongoose.Schema({
+    Email:String,
+    password:String
+});
+
+userSchema.plugin(mongooseFieldEncryption, {
+    fields: ["password"],
+    secret: process.env.SECRET
+});
+
+const User = mongoose.model("User",userSchema);
+
+app.get("/",function(req,res){
+    res.render("home")
+})
+
+app.get("/login",function(req,res){
+    res.render("login")
+})
+
+app.get("/register",function(req,res){
+    res.render("register")
+})
+
+app.post("/register",function(req,res){
+    const newUser=new User({
+        Email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function(err){
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.render("secrets")
+        }
+    });
+})
+
+app.post("/login",function(req,res){
+    const userName=req.body.username;
+    const password=req.body.password;
+
+    User.findOne({Email:userName},function(err,foundUser){
+        if(err){
+            console.log(err)
+        }
+        else if(foundUser){
+            if(password===foundUser.password){
+                res.render("secrets")
+            }
+            else{
+                console.log("Wrong password")
+            }
+        }
+    })
+
+})
+
+app.listen(3000);
